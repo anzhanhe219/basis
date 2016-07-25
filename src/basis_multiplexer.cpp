fdf6f81c49ec4189c8ccb41f7ac1ea67302eef30
anzhh <azh885522@sina.com>
2016-07-25 18:31:21 +0800

    add epoll code

--- src/basis_multiplexer.cpp
+++ src/basis_multiplexer.cpp
@@ -1,6 +1,7 @@
 
 #include "basis_multiplexer.h"
 #include "basis_event_loop.h"
+#include "basis_socket.h"
 
 namespace basis
 {
@@ -100,7 +101,7 @@ namespace basis
 		{
 			int count = 0;
 			data->m_curr_index = 0;
-			data->m_partners.clear();			
+			data->m_fired_fds.clear();			
 			map<int, BSFdPartner*>::iterator it = data->m_partners.begin();
 			for (; it != data->m_partners.end(); ++it)
 			{
@@ -129,7 +130,7 @@ namespace basis
 		else if (nret < 0)
 		{
 			// 错误日志
-			int err = GetLastError();
+			int err = BSSocket::last_error();
 			printf("select error %d.\n", err);
 			return -1;
 		}
@@ -152,7 +153,7 @@ namespace basis
 
 	void BSMultiplexer::Destroy()
 	{
-		delete (BSMultiplexer*)m_private_data;
+		delete (BSWinSelectData*)m_private_data;
 	}
 
 	const char* BSMultiplexer::ApiName()
@@ -166,6 +167,161 @@ namespace basis
 #ifdef __POSIX__
 #ifdef __USE_EPOLL__
 
+	enum {
+		max_epoll_size = 10240,
+		max_epoll_ready = 1024;
+	};
+
+	class BSEpollData
+	{
+	public:
+		BSEpollData() : m_epoll_fd(-1), m_curr_index(0) {}
+
+		int m_epoll_fd;
+		int m_curr_index;
+		vector<int> m_maskes;
+		vector<BSFiredFd> m_fired_fds;
+		struct epoll_event m_polled_events[max_epoll_ready];
+	};
+
+	BSMultiplexer* BSMultiplexer::Create(int sz)
+	{
+		if (sz > max_epoll_size) return NULL;
+
+		BSEpollData* data = new BSEpollData;
+		if (data == NULL) return NULL;
+		do 
+		{
+			if (data->m_epoll_fd = epoll_create(sz) < 0) break;
+			if (BSMultiplexer* mlp = new BSMultiplexer())
+			{
+				data->m_fired_fds.reserve(512);
+				data->m_partners.resize(sz, 0);
+				mlp->m_private_data = data;			
+				return mlp;
+			}
+		} while (false);
+
+		delete data;
+		return NULL;
+	}
+
+	bool BSMultiplexer::AddEvent(int fd, int mask, BSFdPartner* partner)
+	{
+		if (mask == fs_none) return false;
+		if (partner == NULL) return false;
+		BSEpollData* data = (BSEpollData*)m_private_data;	
+		if (fd >= data->m_maskes.size()) return false;
+		int* fd_mask = &data->m_maskes[fd];
+
+		struct epoll_event evt;
+		evt.data.fd = fd;
+		evt.data.ptr = partner;
+		evt.events = *fd_mask;
+		if (mask & fs_readable)	evt.events |= EPOLLIN;
+		if (mask & fs_writeable) evt.events |= EPOLLOUT;
+		int ctl = (*fd_mask) ? EPOLL_CTL_MOD : EPOLL_CTL_ADD;
+
+		if (epoll_ctl(data->m_epoll_fd, ctl, fd, &evt) == -1)
+		{
+			return false;
+		}
+
+		*fd_mask = evt.events; // 记录已经注册事件
+		return true;
+	}
+
+	bool BSMultiplexer::DelEvent(int fd, int mask, BSFdPartner* partner)
+	{
+		if (mask == fs_none) return false;
+		if (partner == NULL) return false;
+		BSEpollData* data = (BSEpollData*)m_private_data;	
+		if (fd >= data->m_maskes.size()) return false;
+		int* fd_mask = &data->m_maskes[fd];
+
+		struct epoll_event evt;
+		evt.data.fd = fd;
+		evt.data.ptr = partner;
+		evt.events = *fd_mask;
+		if (mask & fs_readable)	evt.events &= ~EPOLLIN;
+		if (mask & fs_writeable) evt.events &= ~EPOLLOUT;
+		int ctl = (evt.events) ? EPOLL_CTL_MOD : EPOLL_CTL_DEL;
+
+		if (epoll_ctl(data->m_epoll_fd, ctl, fd, &evt) == -1)
+		{
+			return false;
+		}
+
+		*fd_mask = evt.events;
+		return true;
+	}
+
+	int BSMultiplexer::Poll(BSEventLoop *el,  struct timeval *tvp)
+	{
+		int timeout = tvp ? tvp->tv_sec*1000 + tvp->tv_usec/1000 : -1;
+		BSEpollData* data = (BSEpollData*)m_private_data;	
+		data->m_curr_index = 0;
+		data->m_fired_fds.clear();
+		int nret=epoll_wait(data->m_epoll_fd, data->m_polled_events, max_epoll_ready, timeout); 
+		if (nret > 0)
+		{
+			for (int i = 0; i < nret; ++i)
+			{
+				struct epoll_event* evt = &data->m_polled_events[i];
+
+				int fd = evt->data.fd;
+				BSFdPartner* fp = (BSFdPartner*)evt.data.ptr;
+				if (fp == NULL) continue;
+				if (fp->m_logic_mask == fl_empty) continue;
+
+				int mark = fs_none;
+				if (evt->events & EPOLLIN)
+					mark |= fs_readable;
+				if (evt->events & EPOLLOUT)
+					mark |= fs_writeable;
+				if (mark == fs_none) continue;
+
+				BSFiredFd fr;
+				fr.m_fd = fd;
+				fr.m_partner = fp;
+				fr.m_state_mask = mark;
+				data->m_fired_fds.push_back(fr);
+			}
+			return data->m_fired_fds.size();
+		}
+		else if (nret < 0)
+		{
+			// 错误日志
+			int err = BSSocket::last_error();
+			printf("epoll error %d.\n", err);
+			return -1;
+		}
+		return 0;		
+	}
+
+	BSFiredFd* BSMultiplexer::FetchFiredFd()
+	{
+		BSEpollData* data = (BSEpollData*)m_private_data;
+		if (data->m_curr_index >= data->m_fired_fds.size()) return NULL;		
+		return &data->m_fired_fds[data->m_curr_index++];
+	}
+
+	BSFiredFd* BSMultiplexer::GetFiredFd(uint32 index)
+	{
+		BSEpollData* data = (BSEpollData*)m_private_data;
+		if (index >= data->m_fired_fds.size()) return NULL;		
+		return &data->m_fired_fds[index];
+	}
+
+	void BSMultiplexer::Destroy()
+	{
+		delete (BSEpollData*)m_private_data;
+	}
+
+	const char* BSMultiplexer::ApiName()
+	{
+		return "linux epoll";
+	}
 #else
 #ifdef __USE_POLL__
 
