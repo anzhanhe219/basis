710570bee676e5ad3389b2349dd1f71ef81a5810
anzhanhe <anzhanhe@ixianlai.com>
2017-12-29 10:33:42 +0800

    linux compile

--- include/basis_socket_io.h
+++ include/basis_socket_io.h
@@ -9,12 +9,14 @@ namespace basis
 	class BSSocketIO
 	{
 	public:
-		static int connect_syne(BSSockAddr& addr, int ms);
+		static int connect_sync(BSSockAddr& addr, int ms);
 		static int connect_asyn(BSSockAddr& addr, bool& success);
 		static bool listen(int sock, const BSSockAddr& sockaddr, int backlog);
 		static bool accept(int sock, vector<int>& new_socks);
 		static int write(int sock, void* buffer, uint32 bf_sz);
 		static int read(int sock, void* buff, uint32 max_bf_sz);
+
+		static bool is_would_block(int error);
 	};
 
 } // namespace basis
